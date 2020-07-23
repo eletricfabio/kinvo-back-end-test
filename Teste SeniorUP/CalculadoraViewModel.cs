0f84ebe954a1303f8c1b42b6bd0d073a82214577
Fábio Vieira <43624576+eletricfabio@users.noreply.github.com>
2020-07-23 19:09:47 -0300

    Fábio Vieira
    CalculadoraIR

---
+++ Teste SeniorUP/CalculadoraViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CalculadoraTeste.Models
+{
+    public class CalculadoraViewModel
+    {
+        /// <summary>
+        /// Valor do Resgate da Aplicação
+        /// </summary>
+        [Display(Name = "Valor do Resgate")]
+        public double Num1 { get; set; }
+
+        /// <summary>
+        /// Valor do Resgate da Aplicação
+        /// </summary>
+        ///[Display(Name = "Valor do Resgate")]
+        ///public int Num2 { get; set; }
+
+        /// <summary>
+        /// Operador
+        /// </summary>
+        [Display(Name = "Período da Aplicação")]
+        public int Operador { get; set; }
+
+        /// <summary>
+        /// Resultado do Calculo IR
+        /// </summary>
+        [Display(Name = "Valor com desconto do IR")]
+        public int? Resultado { get; set; }
+    }
+}
